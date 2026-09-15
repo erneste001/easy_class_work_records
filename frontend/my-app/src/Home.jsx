ccab33b670a4baf1534947e433092789c2d7f504
ART <ernesteitangishaka31@gmail.com>
2026-09-15 15:47:55 +0200

    new commiting in the project

--- frontend/my-app/src/Home.jsx
+++ frontend/my-app/src/Home.jsx
@@ -1755,7 +1755,7 @@ export default function EasyClassWork() {
               <img src={logo} className="w-6 h-6 text-[#178754]" aria-hidden="true" />
             </span>
             <span className="ecw-heading font-bold text-[15px] text-neutral-900 leading-none">
-              Easy Class Records System itangishaka
+              Easy Class Records System
             </span>
           </a>
 
